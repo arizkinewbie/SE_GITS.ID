56a55896483692fbb7f497144828f5c050f88571
Arizki Putra Rahman <arizkijitsu@gmail.com>
2023-07-19 02:20:52 +0700

    change files

--- question2.js
+++ question2.js
@@ -1,45 +1,45 @@
 // Read input from stdin
 function readInput() {
-  const rl = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  const lines = [];
-  return new Promise((resolve) => {
-    rl.on('line', (line) => {
-      lines.push(line);
-    }).on('close', () => {
-      //Separate player score and GITS score
-      const playerScores = lines[1].split(' ').map(Number);
-      const gitsScores = lines[3].split(' ').map(Number);
-      resolve({ playerScores, gitsScores });
-    });
-  });
+	const rl = require('readline').createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	const lines = [];
+	return new Promise((resolve) => {
+		rl.on('line', (line) => {
+			lines.push(line);
+		}).on('close', () => {
+			//Separate player score and GITS score
+			const playerScores = lines[1].split(' ').map(Number);
+			const gitsScores = lines[3].split(' ').map(Number);
+			resolve({
+				playerScores,
+				gitsScores
+			});
+		});
+	});
 }
 
 function denseRanking(scores, gitsScores) {
-  // Menghapus nilai-nilai duplikat dari daftar skor pemain
-  const uniqueScores = Array.from(new Set(scores)).sort((a, b) => b - a);
-  const rankings = [];
-
-  for (const gitsScore of gitsScores) {
-    // Menggabungkan skor GITS dengan daftar skor pemain dan mengurutkannya
-    const combinedScores = [...uniqueScores, gitsScore].sort((a, b) => b - a);
-    // Mencari peringkat GITS dalam daftar skor yang diurutkan
-    const rank = combinedScores.indexOf(gitsScore) + 1;
-    rankings.push(rank);
-  }
-
-  return rankings;
+	const uniqueScores = Array.from(new Set(scores)).sort((a, b) => b - a);
+	const rankings = [];
+	////Merge the GITS scores with the list of player scores and sort them
+	for (const gitsScore of gitsScores) {
+		const combinedScores = [...uniqueScores, gitsScore].sort((a, b) => b - a);
+		const rank = combinedScores.indexOf(gitsScore) + 1;
+		rankings.push(rank);
+	}
+	return rankings;
 }
 
 async function main() {
-  const { playerScores, gitsScores } = await readInput();
-
-  // Memanggil fungsi denseRanking dengan input yang telah diproses
-  const result = denseRanking(playerScores, gitsScores);
-  console.log(result.join(' '));
+	const {
+		playerScores,
+		gitsScores
+	} = await readInput();
+	const result = denseRanking(playerScores, gitsScores);
+	console.log(result.join(' '));
 }
 
-// Panggil fungsi utama
+// call main function
 main();
--- question3.js
+++ question3.js
@@ -1,33 +1,33 @@
 // Balance Bracket Problem
 function BalancedBracket(inputString) {
-    const stack = [];
-    const bracketMap = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    };
+	const stack = [];
+	const bracketMap = {
+		')': '(',
+		'}': '{',
+		']': '['
+	};
 
-    for (let char of inputString) {
-        if (bracketMap[char]) {
-            if (!stack.length || stack.pop() !== bracketMap[char]) {
-                return "NO";
-            }
-        } else {
-            stack.push(char);
-        }
-    }
+	for (let char of inputString) {
+		if (bracketMap[char]) {
+			if (!stack.length || stack.pop() !== bracketMap[char]) {
+				return "NO";
+			}
+		} else {
+			stack.push(char);
+		}
+	}
 
-    return stack.length === 0 ? "YES" : "NO";
+	return stack.length === 0 ? "YES" : "NO";
 }
 
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
 readline.question("Input: ", function(input) {
-    const inputString = input.replace(/\s+/g, '');
-    const result = BalancedBracket(inputString);
-    console.log(`Output: ${result}`);
-    readline.close();
+	const inputString = input.replace(/\s+/g, '');
+	const result = BalancedBracket(inputString);
+	console.log(`Output: ${result}`);
+	readline.close();
 });
