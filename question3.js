d920d00a64f09b17edc5578c5a8855edcb6fe2d4
Arizki Putra Rahman <arizkijitsu@gmail.com>
2023-07-19 02:10:40 +0700

    add question 3

---
+++ question3.js
@@ -0,0 +1,33 @@
+// Balance Bracket Problem
+function BalancedBracket(inputString) {
+    const stack = [];
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of inputString) {
+        if (bracketMap[char]) {
+            if (!stack.length || stack.pop() !== bracketMap[char]) {
+                return "NO";
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0 ? "YES" : "NO";
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("Input: ", function(input) {
+    const inputString = input.replace(/\s+/g, '');
+    const result = BalancedBracket(inputString);
+    console.log(`Output: ${result}`);
+    readline.close();
+});
